db390806b2a55cc3321a96360fed0437d204a7c4
runarberg <runarberg@zoho.com>
2015-08-08 17:07:57 +0000

    v0.3.1

--- dist/ascii2mathml.js
+++ dist/ascii2mathml.js
@@ -1,4 +1,4 @@
-/*! ascii2mathml v0.3.0 | (c) 2015 (MIT) | https://github.com/runarberg/ascii2mathml#readme */
+/*! ascii2mathml v0.3.1 | (c) 2015 (MIT) | https://github.com/runarberg/ascii2mathml#readme */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ascii2mathml = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
@@ -1029,7 +1029,7 @@ function parser(options) {
       if (!cell || cell.length === 0) {
         return [mtd(acc), ""];
       }
-      if (cell[0] === ",") {
+      if (cell[0] === options.colSep) {
         return [mtd(acc), cell.slice(1).trim()];
       }
 
