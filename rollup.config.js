a130dfa820f1c308c7aeecbdf7ddacaff362216e
Rúnar Berg <runarberg@zoho.com>
2019-12-31 13:36:55 -0800

    Bundle dependencies
    So that we can run `.mjs` files both in a browser and node
    environments.

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,48 @@
 import babel from "rollup-plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 const {
   npm_package_name: NAME,
   npm_package_version: VERSION,
   npm_package_homepage: HOMEPAGE,
-  npm_package_license: LICENSE
+  npm_package_license: LICENSE,
 } = process.env;
 const YEAR = new Date().getFullYear();
 
-export default {
+const dependenciesConfig = {
+  input: "dependencies.src.mjs",
+  output: [
+    {
+      file: `dependencies.mjs`,
+      format: "esm",
+    },
+  ],
+  plugins: [resolve(), commonjs()],
+};
+
+const defaultConfig = {
   input: "src/index.js",
   output: [
     {
-      file: `target/node/${NAME}.js`,
+      file: `target/node/${NAME}.cjs`,
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: `target/browser/${NAME}.js`,
       format: "iife",
       name: NAME,
       banner: `/*! ${NAME} v${VERSION} | (c) 2015-${YEAR} (${LICENSE}) | ${HOMEPAGE} */`,
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: `target/browser/${NAME}.min.js`,
       format: "iife",
       name: NAME,
-      plugins: [terser()]
-    }
+      plugins: [terser()],
+    },
   ],
   plugins: [
     babel({
@@ -39,10 +52,18 @@ export default {
           "@babel/preset-env",
           {
             modules: false,
-            targets: "last 2 versions, not dead"
-          }
-        ]
-      ]
-    })
-  ]
+            targets: "last 2 versions, not dead",
+          },
+        ],
+      ],
+    }),
+  ],
 };
+
+export default function rollup(args) {
+  if (args.configDeps === true) {
+    return dependenciesConfig;
+  }
+
+  return [dependenciesConfig, defaultConfig];
+}
---
+++ rollup.dependencies.config.js
@@ -0,0 +1,1 @@
+/* eslint-disable import/no-extraneous-dependencies */
