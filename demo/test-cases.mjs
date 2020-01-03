f6c0309507e8fe7a0169fd09cf97b7e7405dff34
Rúnar Berg <runarberg@zoho.com>
2020-01-02 22:06:57 -0800

    Package mathup inside a web component
    And use in the demo

--- demo/test-cases.mjs
+++ demo/test-cases.mjs
@@ -1,12 +1,51 @@
-/* eslint-disable import/no-absolute-path, import/no-unresolved */
-import mathup from "/src/index.mjs";
-
-const testCases = document.getElementById("test-cases");
-
-for (const testCase of testCases.querySelectorAll(".test-case")) {
-  const conf = { ...testCase.dataset };
-  const input = testCase.querySelector("code").textContent;
-  const output = testCase.querySelector("output");
-
-  output.appendChild(mathup(input, conf).toDOM());
+const STYLE_CONTENT = `
+.test-case {
+  font-size: 1.25em;
+  margin-block-end: 2em;
 }
+
+.test-case pre {
+  background: #ffc;
+  border: 1px dashed black;
+  border-radius: 0.2em;
+  max-width: calc(100% - 2em);
+  overflow-x: auto;
+  padding: 0.5ex 0.5em;
+  width: max-content;
+}
+
+.test-case output::before {
+  content: "⇒";
+  font-size: 1.5em;
+  margin-inline-end: 1em;
+}
+`;
+
+class TestCaseElement extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const shadowStyle = document.createElement("style");
+    shadowStyle.textContent = STYLE_CONTENT;
+    shadow.appendChild(shadowStyle);
+
+    const shadowRoot = document.createElement("div");
+    const pre = document.createElement("pre");
+    const code = document.createElement("code");
+    const mathUp = document.createElement("math-up");
+    const input = this.textContent.trim();
+
+    code.textContent = input;
+    mathUp.textContent = input;
+
+    pre.appendChild(code);
+    shadowRoot.appendChild(pre);
+    shadowRoot.appendChild(mathUp);
+    shadowRoot.className = "test-case";
+    shadow.appendChild(shadowRoot);
+  }
+}
+
+customElements.define("test-case", TestCaseElement);
