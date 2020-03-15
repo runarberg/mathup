082657bae949fbb976a05aa3c981185227e73255
Rúnar Berg <runarberg@zoho.com>
2020-03-15 09:38:07 -0700

    Add polyfill to doc page

--- docs/main.mjs
+++ docs/main.mjs
@@ -5,17 +5,38 @@ const tryOutput = document.getElementById("try-output");
 const tryDecimalMark = document.getElementById("try-decimalmark");
 const tryColSep = document.getElementById("try-colsep");
 const tryRowSep = document.getElementById("try-rowsep");
+const tryPolyfill = document.getElementById("try-polyfill");
 const tryMathUp = tryOutput.querySelector("math-up");
 
-tryMathUp.display = tryDisplay.checked ? tryDisplay.value : null;
-tryMathUp.dir = tryDir.checked ? tryDir.value : null;
+tryMathUp.display = tryDisplay.checked ? tryDisplay.value : "";
+tryMathUp.dir = tryDir.checked ? tryDir.value : "";
 tryMathUp.decimalMark = tryDecimalMark.value;
 tryMathUp.colSep = tryColSep.value;
 tryMathUp.rowSep = tryRowSep.value;
 tryMathUp.textContent = tryInput.value;
 
-tryInput.addEventListener("input", event => {
+if (tryPolyfill.checked) {
+  setupPolyfill();
+}
+
+tryInput.addEventListener("input", async event => {
   tryMathUp.textContent = event.target.value;
+
+  if (!tryPolyfill.checked) {
+    return;
+  }
+
+  for (const node of tryOutput.querySelectorAll("mjx-container, math")) {
+    node.remove();
+  }
+
+  await beforeAnimationFrame();
+  await beforeAnimationFrame();
+
+  const mathNode = tryMathUp.shadowRoot.querySelector("math");
+
+  tryOutput.appendChild(document.importNode(mathNode, true));
+  window.MathJax.typeset();
 });
 
 tryDecimalMark.addEventListener("change", event => {
@@ -33,11 +54,79 @@ tryRowSep.addEventListener("change", event => {
 tryDisplay.addEventListener("change", event => {
   const { checked, value } = event.target;
 
-  tryMathUp.display = checked ? value : null;
+  tryMathUp.display = checked ? value : "";
 });
 
 tryDir.addEventListener("change", event => {
   const { checked, value } = event.target;
 
-  tryMathUp.dir = checked ? value : null;
+  tryMathUp.dir = checked ? value : "";
 });
+
+tryPolyfill.addEventListener("change", event => {
+  const { checked } = event.target;
+
+  if (checked) {
+    setupPolyfill();
+  } else {
+    removePolyfill();
+  }
+});
+
+function beforeAnimationFrame() {
+  return new Promise(resolve => requestAnimationFrame(resolve));
+}
+
+const MATHJAX_SRC = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/mml-chtml.js";
+
+async function setupPolyfill() {
+  for (const example of document.querySelectorAll('use-example')) {
+    if (!example.parentNode.querySelector("mjx-container, math")) {
+      const mathUpNode = example.shadowRoot.querySelector("math-up");
+      const mathNode = mathUpNode.shadowRoot.querySelector("math");
+
+      if (mathNode) {
+        mathUpNode.style.display = "none";
+        example.parentNode.appendChild(document.importNode(mathNode, true));
+      }
+    }
+  }
+
+  if (!tryOutput.querySelector("mjx-contaier, math")) {
+    const mathNode = tryMathUp.shadowRoot.querySelector("math");
+    tryOutput.appendChild(document.importNode(mathNode, true));
+  }
+
+  if (!window.MathJax) {
+    if (document.querySelector(`script[src="${MATHJAX_SRC}"]`)) {
+      await beforeAnimationFrame();
+      await new Promise(resolve => window.setTimeout(resolve, 20));
+      setupPolyfill();
+      return;
+    }
+
+    tryMathUp.style.display = "none";
+
+    const script = document.createElement("script");
+
+    script.src = MATHJAX_SRC;
+    script.async = true;
+    document.head.appendChild(script);
+    setupPolyfill();
+    return;
+  }
+
+  window.MathJax.typeset();
+}
+
+function removePolyfill() {
+  tryMathUp.style.display = null;
+
+  for (const mjxContainer of tryOutput.querySelectorAll("mjx-container")) {
+    mjxContainer.remove();
+  }
+
+  for (const mathNode of tryOutput.querySelectorAll("math")) {
+    mathNode.remove();
+  }
+}
