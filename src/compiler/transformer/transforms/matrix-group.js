17556b40e989f3a9dfb102b47bd0fd4965435fd6
Rúnar Berg <runarberg@zoho.com>
2023-11-04 16:05:59 +0000

    Left align matrices without end param
    **Output Changes**
    
    - Now matrices without an end param (such as cases or groups of
      equations) are automatically left aligned.
    - Operators that start a cell content have their left space
      stripped. This is to ensure left alignment holds.
    
    Resolves: #87

--- src/compiler/transformer/transforms/matrix-group.js
+++ src/compiler/transformer/transforms/matrix-group.js
@@ -1,9 +1,11 @@
 /**
+ * @typedef {import("../../parser/index.js").Node} Node
  * @typedef {import("../../parser/index.js").MatrixGroup} MatrixGroup
+ * @typedef {import("../index.js").Tag} Tag
  */
 
 /**
- * @param {import("../index.js").Tag | null} tag
+ * @param {Tag | null} tag
  * @returns {boolean}
  */
 function notNull(tag) {
@@ -16,6 +18,16 @@ function notNull(tag) {
 export default function matrixGroup(node, transform) {
   const childNodes = [];
 
+  const colCount = node.items.reduce(
+    (max, row) => Math.max(max, row.length),
+    0,
+  );
+
+  // auto start-align tables with no close param.
+  const aligns = !node.attrs.close
+    ? Array.from({ length: colCount }).fill("start")
+    : [];
+
   if (node.attrs.open) {
     childNodes.push({
       tag: "mo",
@@ -26,13 +38,42 @@ export default function matrixGroup(node, transform) {
     });
   }
 
+  /**
+   * @param {Node} cell
+   * @param {number} i
+   * @returns {Tag | null}
+   */
+  function transformCell(cell, i) {
+    if (i > 0) {
+      return transform(cell);
+    }
+
+    // Leading operators sometimes have an annoying leading space.
+    if (cell.type === "Term" && cell.items[0]?.type === "OperatorLiteral") {
+      const [first, ...rest] = cell.items;
+
+      if (!first) {
+        return transform(cell);
+      }
+
+      const { attrs = {} } = first;
+      return transform({
+        ...cell,
+        items: [{ ...first, attrs: { ...attrs, lspace: "0" } }, ...rest],
+      });
+    }
+
+    return transform(cell);
+  }
+
   childNodes.push({
     tag: "mtable",
     childNodes: node.items.map((row) => ({
       tag: "mtr",
-      childNodes: row.map((col) => ({
+      childNodes: row.map((col, i) => ({
         tag: "mtd",
-        childNodes: col.map(transform).filter(notNull),
+        attrs: aligns[i] ? { style: `text-align: ${aligns[i]}` } : {},
+        childNodes: col.map(transformCell).filter(notNull),
       })),
     })),
   });
